b31f05a33459277c60dfb1b727aee52b20400cc0
oliverfaris <oliver.faris@menloschool.org>
2026-01-18 21:15:26 -0800

    v2!

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,26 +1,68 @@
+import java.util.ArrayList;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Oliver Faris
  */
 
 public class WeatherPatterns {
-
-
     /**
      * Longest Warming Trend
      * @param temperatures
      * @return the longest run of days with increasing temperatures
      */
+    private static ArrayList<Integer>[] adjacencyList;
+    private static int[] longest;
+
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
+        adjacencyList = new ArrayList[temperatures.length];
+        longest = new int[temperatures.length];
 
-        // I am a good programmer :)
+        // Initialize every arraylist
+        for (int i = 0; i < adjacencyList.length; i++) {
+            adjacencyList[i] = new ArrayList<>();
+        }
+        int count;
 
-        // yes i am
+        // Fill adjacency list
+        for (int i = 0; i < adjacencyList.length; i++) {
+            count = i;
+            while (count > 0) {
+                count--;
+                int temp = temperatures[count];
+                if (temp < temperatures[i])
+                    adjacencyList[i].add(count);
+            }
+        }
 
-        return 0;
+        // Find longest path to each node
+        int longestLength = -1;
+        for (int i = 0; i < temperatures.length; i++) {
+            int length = longestPathTo(i);
+            if (length > longestLength)
+                longestLength = length;
+        }
+
+        return longestLength;
+    }
+
+    public static int longestPathTo(int index) {
+        int len = 1;
+
+        // If longest length isn't saved yet find the longest length
+        if (longest[index] == 0) {
+            for (int in : adjacencyList[index]) {
+                len = Math.max(len, longestPathTo(in) +1);
+            }
+            // Save to longest length array
+            longest[index] = len;
+        }
+        else
+            len = longest[index];
+
+        return len;
     }
 }
